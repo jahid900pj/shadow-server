0e6ebd3d6911eca12c6a2ff981f4657a73d93797
jahidhasan234031 <jahidhasan234031@mail.com>
2023-01-01 09:08:15 +0600

    done

--- index.js
+++ index.js
@@ -12,32 +12,42 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('shadow with you')
 })
-// flow-shadow
-// rS7KDKARwSw4cf4H
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6l0by.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// console.log(uri)
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
 
-// const doctorsCollections = client.db('doctorsPortal').collection('doctors')
-// app.post('/users', async (req, res) => {
-//     const user = req.body;
-//     const result = await usersCollections.insertOne(user)
-//     res.send(result)
-// })
+
+function verifyJWT(req, res, next) {
+    // console.log('token vvv', req.headers.authorization)
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        return res.status(401).send('unauthorize access')
+    }
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access1' })
+        }
+        req.decoded = decoded
+        next()
+    })
+
+}
+
+
+
 async function run() {
     try {
         // await client.connect()
-        // console.log('Db connected'.yellow)
+        // console.log('Db connected')
 
         const postCollections = client.db('Flow-shadow').collection('posts')
+        const reviewCollection = client.db('Flow-shadow').collection('review')
+        const aboutCollection = client.db('Flow-shadow').collection('about')
 
         // Add post 
         app.post('/post', async (req, res) => {
@@ -52,12 +62,20 @@ async function run() {
             const posts = await postCollections.find(query).toArray()
             res.send(posts)
         })
-        // app.get('/bikeCollections/:category_id', async (req, res) => {
-        //     const id = req.params.category_id
-        //     const query = { category_id: id }
-        //     const result = await bikeCollections.find(query).toArray()
-        //     res.send(result)
-        // })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollections.findOne(query)
+            // console.log(user)
+            // res.send({ accessToken: "token" })
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '12h' });
+                // console.log(process.env.ACCESS_TOKEN)
+                return res.send({ accessToken: token })
+            }
+
+        })
 
         // detail post 
         app.get('/post/:id', async (req, res) => {
@@ -67,6 +85,46 @@ async function run() {
             const result = await postCollections.find(query).toArray()
             res.send(result)
         })
+
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+
+        app.get('/addReviews', verifyJWT, async (req, res) => {
+            let query = {}
+            if (req.query.serviceId) {
+                query = {
+                    serviceId: req.query.serviceId
+                }
+            }
+            const cursor = reviewCollection.find(query)
+            const review = await cursor.toArray()
+            res.send(review)
+
+        })
+
+        app.get('/reviews', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query)
+            const review = await cursor.toArray()
+            res.send(review)
+
+        })
+
+
+
+
+
+
+
 
 
 
